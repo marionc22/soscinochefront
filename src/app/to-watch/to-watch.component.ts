0f29d03667f4338dff02988e256de5f16f9eaaa2
CORBEL Marion (SNCF VOYAGEURS / Direction Intercites / EQUIPE 1 ESCALE PAZ) <9301738k@commun.ad.sncf.fr>
2022-11-22 15:44:22 +0100

    marion

--- src/app/to-watch/to-watch.component.ts
+++ src/app/to-watch/to-watch.component.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './to-watch.component.html',
   styleUrls: ['./to-watch.component.css']
 })
-export class ToWatchComponent {
+export class ToWatchComponent implements OnInit {
 
   username : String = "reyducul"
 
@@ -14,9 +14,29 @@ export class ToWatchComponent {
   
   constructor(private http: HttpClient) {
     http.get('http://localhost:8080/towatch').subscribe(data => this.toWatchList = data);
+    
+  }
+  ngOnInit(){
   }
 
-  deleteWatched (id: Number) {
+  addWatched (movieId:number, id: number) {
+    this.http.post('http://localhost:8080/watched',
+    {username: this.username,movie: {id: movieId}},
+).subscribe(
+  response => {
+      console.log("POST call in error", response);
+  },
+  () => {
+      console.log("The POST observable is now completed.");
+  });
+    this.http.delete(`http://localhost:8080/towatch/${id}`)
+    .subscribe(() => 'Delete successful');
+    this.toWatchList = this.toWatchList.filter((movie: any) => movie.id != id)
+    
+}
+  
+
+    deleteWatched (id: Number) {
     
     this.http.delete(`http://localhost:8080/towatch/${id}`)
     .subscribe(() => 'Delete successful');
